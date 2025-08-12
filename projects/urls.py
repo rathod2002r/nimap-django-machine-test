9a87b1a4b804effb5e46a611c4e9f667112c973a
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:15:19 +0530

    urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,1 +1,11 @@
 
+from django.urls import path
+from .views import ProjectViewSet
+
+project_list = ProjectViewSet.as_view({'get': 'list'})
+project_create = ProjectViewSet.as_view({'post': 'create'})
+
+urlpatterns = [
+    path('', project_list, name='project-list'),
+    path('clients/<int:client_pk>/projects/', project_create, name='project-create'),
+]
