9b5ebc1c7050210b29ef19e57a07b42edf07d7e7
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:12:47 +0530

    views.py

--- clients/views.py
+++ clients/views.py
@@ -1,1 +1,16 @@
+from rest_framework import viewsets, permissions
+from .models import Client
+from .serializers import ClientSerializer, ClientDetailSerializer
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ClientDetailSerializer
+        return ClientSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
 
