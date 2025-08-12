807d863f940d90745e3a1f5c19188d4603619609
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:11:49 +0530

    models.py

--- clients/models.py
+++ clients/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='clients_created')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.client_name
 
