da1ef14f963cca5928bf7966a233110207abd535
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:12:21 +0530

    serializers.py

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,1 +1,19 @@
 
+from rest_framework import serializers
+from .models import Client
+from projects.serializers import ProjectSerializer
+
+class ClientSerializer(serializers.ModelSerializer):
+    created_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Client
+        fields = ['id', 'client_name', 'created_at', 'created_by', 'updated_at']
+
+class ClientDetailSerializer(serializers.ModelSerializer):
+    created_by = serializers.StringRelatedField(read_only=True)
+    projects = ProjectSerializer(many=True, read_only=True, source='project_set')
+
+    class Meta:
+        model = Client
+        fields = ['id', 'client_name', 'projects', 'created_at', 'created_by', 'updated_at']
