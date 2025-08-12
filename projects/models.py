4bf8d34d19dc663a0eebab1ee1b99931685be21c
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:13:50 +0530

    models.py

--- projects/models.py
+++ projects/models.py
@@ -1,1 +1,14 @@
 
+from django.db import models
+from django.contrib.auth.models import User
+from clients.models import Client
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=255)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='projects_created')
+
+    def __str__(self):
+        return self.project_name
