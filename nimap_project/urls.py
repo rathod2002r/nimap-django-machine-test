0941ee27f5cf3fc9001b0dcf5fdfe9bdb4aecf61
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:11:11 +0530

    urls.py

--- nimap_project/urls.py
+++ nimap_project/urls.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('clients/', include('clients.urls')),
+    path('projects/', include('projects.urls')),
+]
 
