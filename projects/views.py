c3f8984689ef59a2cfb9b028ca7f80da2628cb6f
Rathod Rahul <156564332+rathod2002r@users.noreply.github.com>
2025-08-13 04:14:53 +0530

    views.py

--- projects/views.py
+++ projects/views.py
@@ -1,1 +1,26 @@
 
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Project
+from .serializers import ProjectSerializer, ProjectCreateSerializer
+from clients.models import Client
+from rest_framework.decorators import action
+
+class ProjectViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        projects = Project.objects.filter(users=request.user)
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, client_pk=None):
+        try:
+            client = Client.objects.get(pk=client_pk)
+        except Client.DoesNotExist:
+            return Response({'detail': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProjectCreateSerializer(data=request.data, context={'client': client, 'request': request})
+        serializer.is_valid(raise_exception=True)
+        project = serializer.save()
+        return Response(ProjectSerializer(project).data, status=status.HTTP_201_CREATED)
